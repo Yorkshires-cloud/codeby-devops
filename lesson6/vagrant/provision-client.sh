d9efd95b56807f62be73d4a562b7ac77337029fe
Yorkshires-cloud <ognev.igor.alex@gmail.com>
2024-03-29 23:54:51 +0700

    update provisions (not working)

--- lesson6/vagrant/provision-client.sh
+++ lesson6/vagrant/provision-client.sh
@@ -1,10 +1,7 @@
+sudo apt update
+sudo apt install sshpass
 sudo /bin/su -c "echo '192.168.1.201 server' >> /etc/hosts"
-sudo ssh-keygen
-/n
-1234
-1234
-sudo ssh-copy-id vagrant@server
-yes
-vagrant
-sudo /bin/su -c "echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config"
+sudo ssh-keygen -P "1234" -f /vagrant/.ssh/id_rsa1
+sudo cat /vagrant/.ssh/id_rsa1.pub | sshpass -p "vagrant" ssh vagrant@server "mkdir -p /vagrant/.ssh && touch /vagrant/.ssh/authorized_keys && chmod 777 go= /vagrant/.ssh && cat >> /vagrant/.ssh/authorized_keys"
+sudo /bin/su -c "echo 'PasswordAuthentication no' > /etc/ssh/sshd_config"
 sudo systemctl restart ssh*

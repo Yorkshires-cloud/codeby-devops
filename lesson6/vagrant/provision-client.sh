5c4106cd1f49fa2d552924e1d8500030f5926b1e
Yorkshires-cloud <ognev.igor.alex@gmail.com>
2024-04-01 22:05:49 +0700

    update provision shell (working)

--- lesson6/vagrant/provision-client.sh
+++ lesson6/vagrant/provision-client.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt install sshpass
 sudo /bin/su -c "echo '192.168.1.201 server' >> /etc/hosts"
-sudo ssh-keygen -P "1234" -f /vagrant/.ssh/id_rsa1
-sudo cat /vagrant/.ssh/id_rsa1.pub | sshpass -p "vagrant" ssh vagrant@server "mkdir -p /vagrant/.ssh && touch /vagrant/.ssh/authorized_keys && chmod 777 go= /vagrant/.ssh && cat >> /vagrant/.ssh/authorized_keys"
-sudo /bin/su -c "echo 'PasswordAuthentication no' > /etc/ssh/sshd_config"
+sudo ssh-keygen -P "1234" -f /home/vagrant/.ssh/id_rsa
+sudo sshpass -p "vagrant" ssh-copy-id -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no vagrant@server
+sudo /bin/su -c "echo 'PubkeyAuthentication yes' > /etc/ssh/sshd_config"
+sudo /bin/su -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
 sudo systemctl restart ssh*
--- lesson6/vagrant/provision-server.sh
+++ lesson6/vagrant/provision-server.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt install sshpass
 sudo /bin/su -c "echo '192.168.1.200 client' >> /etc/hosts"
-sudo ssh-keygen -P "1234" -f /vagrant/.ssh/id_rsa2
-sudo cat /vagrant/.ssh/id_rsa2.pub | sshpass -p "vagrant" ssh vagrant@client "mkdir -p /vagrant/.ssh && touch /vagrant/.ssh/authorized_keys && chmod 777 go= /vagrant/.ssh && cat >> /vagrant/.ssh/authorized_keys"
-sudo /bin/su -c "echo 'PasswordAuthentication no' > /etc/ssh/sshd_config"
+sudo ssh-keygen -P "1234" -f /home/vagrant/.ssh/id_rsa
+sudo sshpass -p "vagrant" ssh-copy-id -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no vagrant@client
+sudo /bin/su -c "echo 'PubkeyAuthentication yes' > /etc/ssh/sshd_config"
+sudo /bin/su -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config"
 sudo systemctl restart ssh*
